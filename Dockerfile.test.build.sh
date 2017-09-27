3f151805de02564354b9d941b9611cc024ceb83b
Tris Setiawan <tris.setiawan@yahoo.com>
2017-09-27 22:19:04 +0700

    update test.build script

--- Dockerfile.test.build.sh
+++ Dockerfile.test.build.sh
@@ -1,6 +1,6 @@
-rmdir -rf "bin/publish"
+rmdir -rf bin/publish
 docker build -f Dockerfile.test.build -t com-moonlay-service-auth-webapi:test-build .
 docker rm com-moonlay-service-auth-webapi-test-build-container
 docker create --name com-moonlay-service-auth-webapi-test-build-container com-moonlay-service-auth-webapi:test-build
 docker cp com-moonlay-service-auth-webapi-test-build-container:/out ./bin/publish
-docker build -f ./Dockerfile.test -t com-moonlay-service-auth-webapi:test .
+docker build -f Dockerfile.test -t com-moonlay-service-auth-webapi:test .
