6629cb47783ef72ae4fb1f7ddebfba463def64c1
Tris Setiawan <tris.setiawan@yahoo.com>
2017-09-28 13:29:31 +0700

    update travis

--- Dockerfile.test.build.sh
+++ Dockerfile.test.build.sh
@@ -1,6 +1,5 @@
-rmdir -rf bin/publish
-docker build -f Dockerfile.test.build -t com-moonlay-service-auth-webapi:test-build .
-docker rm com-moonlay-service-auth-webapi-test-build-container
+docker build -f Dockerfile.test.build -t com-moonlay-service-auth-webapi:test-build . 
 docker create --name com-moonlay-service-auth-webapi-test-build-container com-moonlay-service-auth-webapi:test-build
-docker cp com-moonlay-service-auth-webapi-test-build-container:/out ./bin/publish
+mkdir -p ./bin/publish
+docker cp com-moonlay-service-auth-webapi-test-build-container:/out/. ./bin/publish
 docker build -f Dockerfile.test -t com-moonlay-service-auth-webapi:test .
