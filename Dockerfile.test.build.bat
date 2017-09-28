29bc05f35172e550eb3c652ddf8ac5c506b17571
Tris Setiawan <tris.setiawan@yahoo.com>
2017-09-28 13:07:24 +0700

    update docker

--- Dockerfile.test.build.bat
+++ Dockerfile.test.build.bat
@@ -1,6 +1,7 @@
-rmdir "bin/publish" /s /q
+rmdir "bin" /s /q
 docker build -f Dockerfile.test.build -t com-moonlay-service-auth-webapi:test-build .
 docker rm com-moonlay-service-auth-webapi-test-build-container
 docker create --name com-moonlay-service-auth-webapi-test-build-container com-moonlay-service-auth-webapi:test-build
-docker cp com-moonlay-service-auth-webapi-test-build-container:/out/. ./bin/publish
+md bin
+docker cp com-moonlay-service-auth-webapi-test-build-container:/out ./bin/publish
 docker build -f Dockerfile.test -t com-moonlay-service-auth-webapi:test .
