57cd93e46b7f0a74a41619917ff1c84a1e9e250e
Tris Setiawan <tris.setiawan@yahoo.com>
2017-10-13 19:25:20 +0700

    remove container

--- Dockerfile.test.build.bat
+++ Dockerfile.test.build.bat
@@ -5,3 +5,4 @@ docker create --name com-moonlay-service-auth-webapi-test-build-container com-mo
 md bin
 docker cp com-moonlay-service-auth-webapi-test-build-container:/out ./bin/publish
 docker build --no-cache -f Dockerfile.test -t com-moonlay-service-auth-webapi:test .
+docker rm com-moonlay-service-auth-webapi-test-build-container
